f791e8d76003b10497462500b2b32dae524b6bd9
rauleibanez <83990877+rauleibanez@users.noreply.github.com>
2024-11-28 14:36:35 -0500

    implementing response messages

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -21,7 +21,7 @@ const getPost = async (req, res) => {
     res.status(500).json({ message: err.message }); 
   } 
 };
-
+/*
 const createPost = async (req, res) => {
   const { title, body } = req.body;
   try {
@@ -32,16 +32,23 @@ const createPost = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
-/*
-const updatePost = async (req, res) => {
+*/
+
+const createPost = async (req, res) => {
   try {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(post);
+    const { title, body } = req.body;
+    const post = new Post({
+      title,
+      body,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
+    await post.save();
+    res.status(201).json({ message: 'Publicación creada con éxito', post });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: 'Error al crear la publicación' });
   }
 };
-*/
 
 const updatePost = async (req, res) => {
    try {
@@ -54,18 +61,33 @@ const updatePost = async (req, res) => {
     post.body = body;
     post.updatedAt = Date.now(); // Actualizar la fecha updatedAt
     await post.save();
-    res.json(post);
+    res.json({ message: 'Publicación actualizada con éxito', post });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: 'Error al actualizar la publicación' });
   }
 };
 
+/*
 const deletePost = async (req, res) => {
   try {
     await Post.findByIdAndDelete(req.params.id);
     res.json({ message: 'Post eliminado' });
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+};
+*/
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Publicación no encontrada' });
+    }
+    await post.remove();
+    res.json({ message: 'Publicación eliminada con éxito' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error al eliminar la publicación' });
   }
 };
 
